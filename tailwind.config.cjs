e9adca22181152b49b9ff28ce8a3b27104860773
dandele <dandele93@gmail.com>
2023-08-28 13:46:36 +0200

    risoluzione definitiva primo logo wall

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -108,6 +108,20 @@ module.exports = {
         sans: ["Space Mono", ...defaultTheme.fontFamily.sans],
 
       },
+      animation: {
+        marquee: 'marquee 30s linear infinite',
+        marquee2: 'marquee2 30s linear infinite'
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' }
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' }
+        }
+      },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
